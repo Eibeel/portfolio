238e928ef0748ebf6d18b2fc830eaf4c97755066
Eibeel <abelguardop@gmail.com>
2023-03-09 19:34:58 -0500

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
 import { Main } from '@/components/layouts/Main';
 import { Chakra } from "@/components/Chakra";
 import { Fonts } from '@/components/Fonts';
+import { Analytics } from '@vercel/analytics/react';
 
 
-export default function App({ Component, pageProps }) {
+export default function App ({ Component, pageProps }) {
   return (
-    <Chakra>
-      <Fonts />
-      <Main>
-        <Component {...pageProps} />
-      </Main>
-    </Chakra>
+    <>
+      <Chakra>
+        <Fonts />
+        <Main>
+          <Component {...pageProps} />
+        </Main>
+      </Chakra>
+      <Analytics />
+    </>
   )
 }
